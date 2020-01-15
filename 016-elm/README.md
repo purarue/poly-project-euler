7aad600e84369e0b89d0b2f481e1e5946a8665e1
Sean Breckenridge <seanbrecke@gmail.com>
2020-01-15 01:31:33 -0800

    problem 16 - elm

---
+++ 016-elm/README.md
@@ -0,0 +1,8 @@
+# [Problem 16](https://projecteuler.net/problem=16)
+
+Language: Elm 0.19.1
+
+##### Requires
+
+- [`elm`](https://elm-lang.org/)
+- [`python`](https://www.python.org/) (used for the webbrowser module to launch the webpage)
---
+++ 016-elm/src/Main.elm
@@ -0,0 +1,67 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, text, div)
+
+import Maybe exposing (withDefault)
+import String exposing (length, left, dropLeft)
+
+import BigInt exposing (BigInt, toString, fromInt, fromIntString, add, mul, sub, compare)
+
+-- calulates n to the mth power
+nthPower : BigInt -> BigInt -> BigInt
+nthPower n m =
+  nthPowerHelper n n m
+
+-- nthPower, pass the n value and accumulate the value
+nthPowerHelper : BigInt -> BigInt -> BigInt -> BigInt
+nthPowerHelper acc n m =
+  if m == fromInt 1 then acc else nthPowerHelper (mul acc n) n (sub m (fromInt 1))
+
+-- calculates the sum of the digits
+digitSum : BigInt -> BigInt
+digitSum n = 
+  digitSumHelper (fromInt 0) (toString n)
+
+
+digitSumHelper : BigInt -> String -> BigInt
+digitSumHelper acc nStr =
+  if length nStr == 0 then acc else
+    digitSumHelper (withDefault (fromInt 0) (left 1 nStr |> fromIntString) |> add acc) (dropLeft 1 nStr)
+  
+-- calculates the result
+calculate : Int -> Int -> Model
+calculate n m =
+  nthPower (fromInt n) (fromInt m) |> digitSum
+
+
+-- MAIN
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+-- MODEL
+-- The result of the calculation
+
+type alias Model = BigInt
+
+init : Model
+init =
+  calculate 2 1000
+
+
+-- UPDATE
+
+type Msg = Nothing
+
+update : Msg -> Model -> Model
+update msg model =
+  model
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+    div [] [ text (toString model) ]
