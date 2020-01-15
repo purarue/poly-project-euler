c9bbfc5b4ed74a4001db24ca70ede47fd3a8204f
Sean Breckenridge <seanbrecke@gmail.com>
2020-01-14 23:17:51 -0800

    problem 14 - javascript

---
+++ 014-javascript/README.md
@@ -0,0 +1,7 @@
+## [Problem 14](https://projecteuler.net/problem=14)
+
+Language: Node v13.6.0
+
+##### Requires
+
+- [`node`](https://nodejs.org/en/)
---
+++ 014-javascript/main.js
@@ -0,0 +1,41 @@
+class Collatz {
+  constructor(num) {
+    this.num = num
+    this.chain_length = 1
+  }
+
+  done() {
+    return this.num == 1
+  }
+
+
+  next() {
+    if (this.num % 2 == 0) {
+      this.num = this.num / 2
+    } else {
+      this.num = 3 * this.num + 1
+    }
+    this.chain_length++
+  }
+
+  find_chain_length() {
+    while(!this.done()) {
+      this.next()
+    }
+    return this.chain_length
+  }
+}
+
+let longest_chain = 1
+let longest_chain_length = 1
+
+for (let i = 2; i < 1000000; i++) {
+  let i_chain_length = new Collatz(i).find_chain_length()
+  if (i_chain_length > longest_chain_length) {
+    longest_chain = i
+    longest_chain_length = i_chain_length
+  }
+}
+
+console.log(longest_chain)
+
