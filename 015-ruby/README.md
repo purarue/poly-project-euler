9334639821c205a7fd9d2f5ff9f4ab3c5073a251
Sean Breckenridge <seanbrecke@gmail.com>
2020-01-14 23:46:06 -0800

    problem 15 - ruby

---
+++ 015-ruby/README.md
@@ -0,0 +1,7 @@
+## [Problem 15](https://projecteuler.net/problem=15)
+
+Language: ruby 2.7.0
+
+##### Requires
+
+- [`ruby`](https://www.ruby-lang.org/en/)
---
+++ 015-ruby/main.rb
@@ -0,0 +1,11 @@
+# central binomial coefficient
+# https://en.wikipedia.org/wiki/Central_binomial_coefficient
+
+# Implement factorial for built-in Integer type
+class Integer
+  def factorial
+    (1..self).reduce(:*)
+  end
+end
+
+puts (2*20).factorial / 20.factorial ** 2
