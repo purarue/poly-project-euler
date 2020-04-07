594a2776cba3b1cba5ec92be02ffcf54dad61bd1
Sean Breckenridge <seanbrecke@gmail.com>
2020-04-07 14:36:34 -0700

    add problem 25 - Dlang

---
+++ 025-D/README.md
@@ -0,0 +1,7 @@
+## [Problem 25](https://projecteuler.net/problem=25)
+
+Language: DMD64 D Compiler v2.091.0
+
+##### Requires
+
+- [`dmd`](https://dlang.org/)
---
+++ 025-D/main.d
@@ -0,0 +1,42 @@
+import std.stdio;
+import std.format : format;
+import std.bigint : BigInt;
+import std.algorithm.searching : until;
+import std.range.primitives: walkLength;
+
+const ulong STRLEN = 1000;
+
+// from https://tour.dlang.org/tour/en/basics/ranges
+struct FibonacciRange {
+  // start with F0
+  BigInt x = "0", y = "1";
+  int n = 0;
+
+  // range never ends
+  enum empty = false;
+
+  // current front of the list
+  BigInt front() const @property
+  {
+    return x;
+  }
+
+  // remove first element; get next fibonacci number
+  void popFront()
+  {
+    auto t = x;
+    x = y;
+    y = t + y;
+    n = n + 1;
+  }
+}
+
+// drop from range until we reach length 1000
+static bool target_predicate(BigInt a) {
+  return format("%d", a).length >= STRLEN;
+}
+
+void main() {
+  FibonacciRange fib;
+  writeln(format("%d", until!target_predicate(fib).walkLength));
+}
