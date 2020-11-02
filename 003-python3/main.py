34c233eacbe764453210c5d26f065f809b7d4399
Sean Breckenridge <seanbrecke@gmail.com>
2020-11-01 19:19:34 -0800

    python improvements: cache isprime, use generator

--- 003-python3/main.py
+++ 003-python3/main.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python3
 
-from typing import List
+from typing import Iterator
+from functools import lru_cache
 
 
+@lru_cache(maxsize=None)
 def isprime(n: int) -> bool:
-    """
-    Check from 1 -> sqrt(n) + 1
-    We add two below since upper bound of range is exclusive
-
-    >>> isprime(7)
-    True
-    >>> isprime(8)
-    False
-    """
     if n < 2:
         return False
     elif n == 2:
@@ -26,19 +19,18 @@ def isprime(n: int) -> bool:
     return True
 
 
-def find_prime_factors(n: int) -> List[int]:
-    """
-    >>> find_prime_factors(20)
-    [2, 5]
-    >>> find_prime_factors(182)
-    [2, 7, 13]
-    """
-    prime_factors = []
+def find_prime_factors(n: int) -> Iterator[int]:
     for x in range(1, int(n ** 0.5) + 2):
         if n % x == 0 and isprime(x):
-            prime_factors.append(x)
-    return prime_factors
+            yield x
+
+
+def main() -> None:
+    last: int = -1
+    for x in find_prime_factors(600851475143):
+        last = x
+    print(last)
 
 
 if __name__ == "__main__":
-    print(list(find_prime_factors(600851475143))[-1])
+    main()
