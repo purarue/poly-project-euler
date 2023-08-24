978ca96ab649acda637ab5087e377d17ea04fe8e
Sean Breckenridge <seanbrecke@gmail.com>
2023-08-23 23:21:54 -0700

    simplify nim

--- 003-python3/main.py
+++ 003-python3/main.py
@@ -4,15 +4,11 @@ from typing import Iterator
 
 
 def isprime(n: int) -> bool:
-    # (even check)
     if not n & 1 or n < 2:
         return False
-    elif n == 2:
-        return True
-    else:
-        for p in range(3, int(n**0.5) + 1, 2):
-            if n % p == 0:
-                return False
+    for p in range(3, int(n**0.5) + 1, 2):
+        if n % p == 0:
+            return False
     return True
 
 
@@ -23,7 +19,7 @@ def iter_prime_factors(n: int) -> Iterator[int]:
 
 
 def solve(n: int) -> int:
-    return next(reversed(list(iter_prime_factors(n))))
+    return list(iter_prime_factors(n))[-1]
 
 
 if __name__ == "__main__":
--- 033-nim/main.nim
+++ 033-nim/main.nim
@@ -1,77 +1,41 @@
-import sets
-
 type
   Fraction = ref object of RootObj
-    numerator: int
-    denominator: int
-
-proc toStringFrac(frac: Fraction): string =
-  return $frac.numerator & "/" & $frac.denominator
+    num: int
+    den: int
 
 proc simplifyFrac(frac: Fraction): Fraction =
-  for divide_by in 2..frac.denominator:
-    if frac.numerator mod divide_by == 0 and frac.denominator mod divide_by == 0:
-      return simplifyFrac(Fraction(numerator: (frac.numerator / divide_by).int, denominator: (frac.denominator / divide_by).int))
+  for divide_by in 2..frac.den:
+    if frac.num mod divide_by == 0 and frac.den mod divide_by == 0:
+      return simplifyFrac(Fraction(num: (frac.num / divide_by).int,
+          den: (frac.den / divide_by).int))
   return frac
-
-proc divideFrac(frac: Fraction): float =
-  return frac.numerator / frac.denominator
-
-proc sameFraction(orig: Fraction, newFracNum: int, newFracDen: int): bool =
-  return divideFrac(orig) == divideFrac(Fraction(numerator: newFracNum, denominator: newFracDen))
 
 proc isCuriousFraction(frac: Fraction): bool =
   let
-    numTens = (frac.numerator / 10).int
-    numOnes = frac.numerator mod 10
-    denTens = (frac.denominator / 10).int
-    denOnes = frac.denominator mod 10
+    numTens = frac.num div 10
+    numOnes = frac.num mod 10
+    denTens = frac.den div 10
+    denOnes = frac.den mod 10
 
-  # trivial fractons, e.g. 30/50
-  if numOnes == 0 and denOnes == 0:
+  if numOnes == 0 and denOnes == 0: # trivial fractons, e.g. 30/50
     return false
 
-  # left cancelled left
-  if numTens == denTens and sameFraction(frac, numOnes, denOnes):
-      return true
-  # left cancelled right
-  if numTens == denOnes and sameFraction(frac, numOnes, denTens):
-      return true
-  # right cancelled left
-  if numOnes == denTens and sameFraction(frac, numTens, denOnes):
-      return true
-  # right cancelled right
-  if numOnes == denOnes and sameFraction(frac, numTens, denTens):
-      return true
-  return false
+  proc cancel(a, b, c, d: int): bool =
+    return a == b and d != 0 and frac.num / frac.den == c / d
+
+  return cancel(numTens, denTens, numOnes, denOnes) or cancel(numTens, denOnes,
+      numOnes, denTens) or cancel(numOnes, denTens, numTens, denOnes) or cancel(
+      numOnes, denOnes, numTens, denTens)
 
 var
-  found: HashSet[string]
-  resultFracs: seq[Fraction]
-
-init(found)
-resultFracs = newSeq[Fraction]()
+  rNum, rDen: int = 1
 
 # find all curious fractions under with n,m < 100
-# use stringified, simplified fraction to remove duplicate
-# curious fractions
 for den in 10..99:
   for num in 10..(den - 1):
-    let fr = Fraction(numerator: num, denominator: den)
-    if isCuriousFraction(fr):
-      let frStr = toStringFrac(simplifyFrac(fr))
-      if frStr notin found:
-        found.incl(frStr)
-        resultFracs.add(fr)
+    if isCuriousFraction(Fraction(num: num, den: den)):
+      rNum *= num
+      rDen *= den
 
-# calculate product of resulting fractions
-var
-  rNum: int = 1
-  rDen: int = 1
-
-for f in resultFracs:
-  rNum *= f.numerator
-  rDen *= f.denominator
-
-# simplify and print value of denominator
-echo simplifyFrac(Fraction(numerator: rNum, denominator: rDen)).denominator
+# simplify and print value of den
+echo simplifyFrac(Fraction(num: rNum, den: rDen)).den
